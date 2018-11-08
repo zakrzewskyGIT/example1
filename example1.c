894d7c5cdee6a4e4076074a98bda95432474e5ec
zakrzewskyGIT <44866140+zakrzewskyGIT@users.noreply.github.com>
2018-11-08 13:03:06 +0100

    Update example1.c

--- example1.c
+++ example1.c
@@ -1,12 +1,13 @@
 /* example1.c
- *  * Copyright (C) 2007
+ *  * Copyright (C) 2018
  *   *
  *    * This is free software.
  *     */
 #include<stdio.h>
 
 int main(void){
-    printf("Hello world!\n");
+	printf("Hello world!\n");
+	printf("Hello world!\n");
 	printf("Hello world!\n");
 	printf("Hello world!\n");
     return 0;
